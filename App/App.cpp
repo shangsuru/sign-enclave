6634665c9bc1470060160d91c9d4ec26ebeb01f9
shangsuru <helm.henry99@gmail.com>
2021-07-19 07:48:27 +0000

    Remove comments in App.cpp

--- App/App.cpp
+++ App/App.cpp
@@ -10,18 +10,11 @@
 #include "App.h"
 #include "Enclave_u.h"
 
-/* Global EID shared by multiple threads */
 sgx_enclave_id_t global_eid = 0;
 
-/* Initialize the enclave:
- *   Call sgx_create_enclave to initialize an enclave instance
- */
 int initialize_enclave(void)
 {
     sgx_status_t ret = SGX_ERROR_UNEXPECTED;
-
-    /* Call sgx_create_enclave to initialize an enclave instance */
-    /* Debug Support: set 2nd parameter to 1 */
     ret = sgx_create_enclave(ENCLAVE_FILENAME, SGX_DEBUG_FLAG, NULL, NULL, &global_eid, NULL);
     if (ret != SGX_SUCCESS)
     {
@@ -32,22 +25,16 @@ int initialize_enclave(void)
     return 0;
 }
 
-/* OCall functions */
 void ocall_print_string(const char *str)
 {
-    /* Proxy/Bridge will check the length and null-terminate 
-     * the input string to prevent buffer overflow. 
-     */
     printf("%s", str);
 }
 
-/* Application entry */
 int SGX_CDECL main(int argc, char *argv[])
 {
     (void)(argc);
     (void)(argv);
 
-    /* Initialize the enclave */
     if (initialize_enclave() < 0)
     {
         printf("Enter a character before exit ...\n");
@@ -63,7 +50,6 @@ int SGX_CDECL main(int argc, char *argv[])
 
     printf("r = %d\n", r);
 
-    /* Destroy the enclave */
     sgx_destroy_enclave(global_eid);
 
     printf("Info: SampleEnclave successfully returned.\n");
